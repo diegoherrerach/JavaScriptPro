8f841bbee60fe5ac65beebb2f944285ae29d8a21
diegoherrerach <herrera.ch.diego@gmail.com>
2022-01-30 11:36:44 -0500

    Entrar

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,17 @@
-const video= document.querySelector('video')
-const button = document.querySelector('button')
-function MediaPlayer(config){
-   this.media = config.el
-}
-MediaPlayer.prototype.play =function(){
-   this.media.play();
-}
-MediaPlayer.prototype.pause = function() {
-   this.media.pause();
- };
-const player = new MediaPlayer({el:video});
-button.onclick = () =>player.play();
-button.onclick = () => {
-   player.media.paused ? player.play() : player.pause();
- };
+import MediaPlayer from './mediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+
+ const video = document.querySelector('video');
+ const player = new MediaPlayer({ el: video, plugins:[
+   new AutoPlay() ] });
+ const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
+
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
+};
